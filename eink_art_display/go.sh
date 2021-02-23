4f4edd5e3aa10f32481fd4f9b1c65f9dc5371c77
Matt Bachmann <bachmann.matt@gmail.com>
2021-02-22 20:41:44 -0500

    The color display does not clean, but it does need to clear itself

--- eink_art_display/go.sh
+++ eink_art_display/go.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source /home/pi/eink_art_display/.env
+python3 /home/pi/Pimoroni/inky/examples/7color/clear.py
+# Yes, do it twice. Once does not seem to clear it well
+python3 /home/pi/Pimoroni/inky/examples/7color/clear.py
 python3 /home/pi/eink_art_display/find_art.py
