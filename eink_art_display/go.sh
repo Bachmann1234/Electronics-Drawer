2341eb49ba3c9a688a1eac77a9e9937209baffaf
Matt Bachmann <bachmann.matt@gmail.com>
2021-02-22 20:42:26 -0500

    Clarify the clear function is just for color

--- eink_art_display/go.sh
+++ eink_art_display/go.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /home/pi/eink_art_display/.env
+# The black and white one should not call clear
 python3 /home/pi/Pimoroni/inky/examples/7color/clear.py
 # Yes, do it twice. Once does not seem to clear it well
 python3 /home/pi/Pimoroni/inky/examples/7color/clear.py
